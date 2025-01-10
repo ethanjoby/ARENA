b04c40876720709742a0debcf8fb8b1f15bf45aa
ethanjoby <ev9364@pleasantonusd.net>
2025-01-10 11:25:12 -0800

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Testimonials from './Testimonials';
 import HeroSection from './HeroSection';
+import Services from './Services';
 
 function App() {
   return (
     <div className="App">
     <HeroSection/>
+    <Services/>
     <Testimonials />
     </div>
   );
