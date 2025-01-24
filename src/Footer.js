169e3e24c7b2056d4cd830dd5f46ed5cbe8a2a40
ethanjoby <ev9364@pleasantonusd.net>
2025-01-23 16:24:21 -0800

    change

--- src/Footer.js
+++ src/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
             </p>
             <Link
               to="/sign-up"
-              className="inline-flex border-black items-center gap-2 rounded-full border border-white bg-black px-8 py-3 text-white hover:bg-transparent hover:text-black focus:outline-none focus:ring active:bg-white/90 py-12 px-4 sm:px-6 lg:px-8"
+              className="inline-flex border-black items-center gap-2 rounded-full border  bg-black px-8 py-3 text-white hover:bg-transparent hover:text-black focus:outline-none focus:ring active:bg-white/90 py-12 px-4 sm:px-6 lg:px-8"
             >
               <span className="text-sm font-medium">Register</span>
               <svg
