89da2e9fb297f5abb2d4a4195e010d032c1a3520
myusername <mail.to.aryankumar@gmail.com>
2025-02-06 11:20:49 -0800

    nirav calendar

--- src/AdminPage.js
+++ src/AdminPage.js
@@ -796,7 +796,7 @@ const AdminPage = () => {
   ) : activeMeetingTab === 'Aaron' ? (
     <GoogleFormEmbed src="https://calendar.google.com/calendar/embed?src=arena.college.counseling%40gmail.com&ctz=America%2FLos_Angeles" />
   ) : activeMeetingTab === 'Nirav' ? (
-    <GoogleFormEmbed src="https://calendar.google.com/calendar/embed?src=arena.college.counseling%40gmail.com&ctz=America%2FLos_Angeles" />
+    <GoogleFormEmbed src="https://calendar.google.com/calendar/embed?src=niravjais%40gmail.com&ctz=America%2FLos_Angeles" />
   ) : activeMeetingTab === 'Advay' ? (
     <GoogleFormEmbed src="https://calendar.google.com/calendar/embed?src=arena.college.counseling%40gmail.com&ctz=America%2FLos_Angeles" />
   
