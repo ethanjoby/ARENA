5a6a425585f037bdfb5deda73c1bddaa278ed5cb
Aryan Kumar <116935956+akkumar9@users.noreply.github.com>
2026-02-22 21:40:31 -0800

    Refactor App component to return null
    Removed all route definitions and components from the App function.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from './HomePage';
-import AdminPage from './AdminPage';
-import Essays from './Essays';
-import ThankYouPage from "./ThankYouPage";
-import ArenaSignUpForm from './ArenaSignUpForm';
-import AboutUs from './AboutUs';
-import Portal from './Portal';
-import Webinars from './Webinars';
-import BootcampOfferings from './BootcampOfferings';
-import Checkout from './Checkout';
-import CollegePrepFlyer from './CollegePrepFlyer';
-
 function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Define your routes */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/sign-up" element={<ArenaSignUpForm />} />
-        <Route path="/essays" element={<Essays />} />
-        <Route path="/thank-you" element={<ThankYouPage />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/portal" element={<Portal />} />
-        <Route path="/bootcamp" element={<BootcampOfferings />} />
-        <Route path="/webinars" element={<Webinars />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/collegeprepflyer" element={<CollegePrepFlyer />} />
-      </Routes>
-    </Router>
-  );
+  return null;
 }
-
-export default App;
